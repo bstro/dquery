a463c60dfff0c7cf2ac4a871836ced60fd0fe1d6
Brendan Stromberger <brendanstromberger@gmail.com>
2017-10-17 19:01:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,290 @@
+import { requestAsync, mutateAsync } from 'redux-query';
+import { schema, normalize } from 'normalizr';
+
+import mapValues from 'lodash.mapvalues';
+import merge from 'lodash.merge';
+import head from 'lodash.head';
+import compact from 'lodash.compact';
+import flip from 'lodash.flip';
+import reduce from 'lodash.reduce';
+import get from 'lodash.get';
+import omit from 'lodash.omit';
+
+import pluralize from 'pluralize'
+import camelize from 'voca/camel_case'
+
+import queryBuilder from './query-builder.js';
+export const queryFor = queryBuilder;
+
+const ERR18 =
+  "You're attempting to query for an invalid collection type. This means that you're attempting to fetch or mutate data on a collection that redux-query has no matching definition for in vishnu/schemas/model_definitions.js.";
+
+/**
+ * DQueryFactory: the main interface for fetching data through redux-query.
+ * @param {object} modelDefinitions 
+ * @param {string} api
+ * @param {string} version
+ */
+export default function DQueryFactory(
+  modelDefinitions,
+  api,
+  version,
+  enableDebug
+) {
+  const schemas = _createSchemas(modelDefinitions);
+  const configurations = mapValues(schemas, _toQuery);
+
+  // For easier debugging in a CRA app.
+  if (enableDebug) {
+    window._defs = modelDefinitions;
+    window._schemas = schemas;
+    window._configurations = configurations;
+  }
+
+  function createConfigFor(query, body /*, opts*/) {
+    const { fragments: { resource, include, id } } = query;
+    const resourceName = camelize(resource);
+    const url = query.toString();
+    if (!configurations[resourceName]) throw new Error(ERR18);
+    const baseQuery = configurations[resourceName](include, url);
+
+    let optimisticUpdate = {};
+    if (id) {
+      optimisticUpdate = {
+        [resourceName]: entities => {
+          return Object.assign({}, entities, {
+            [id]: Object.assign({}, entities[id], body)
+          });
+        }
+      };
+    }
+
+    return merge({}, baseQuery, {
+      url: api.concat(version || '').concat(url),
+      optimisticUpdate
+    });
+  }
+
+  return {
+    createConfigFor,
+
+    get schemas() {
+      return schemas;
+    },
+
+    get configurations() {
+      return configurations;
+    },
+
+    GET(query, body, opts) {
+      const baseQuery = createConfigFor(...arguments);
+      const baseOptions = { options: { method: 'GET' } };
+      return requestAsync(merge({}, baseQuery, { body }, baseOptions, opts));
+    },
+
+    CREATE(query, body, opts) {
+      const baseQuery = createConfigFor(...arguments);
+      const baseOptions = { options: { method: 'POST' } };
+      return mutateAsync(merge({}, baseQuery, { body }, baseOptions, opts));
+    },
+
+    UPDATE(query, body, opts) {
+      const baseQuery = createConfigFor(...arguments);
+      const baseOptions = { options: { method: 'PATCH' } };
+      return mutateAsync(merge({}, baseQuery, { body }, baseOptions, opts));
+    },
+
+    DELETE(query, body, opts) {
+      const baseQuery = createConfigFor(...arguments);
+      const baseOptions = { options: { method: 'DELETE' } };
+      return mutateAsync(merge({}, baseQuery, { body }, baseOptions, opts));
+    }
+  };
+}
+
+/* DYNAMIC QUERIES */
+
+function _walkThroughSchemas(prev, cur) {
+  // after reduction:
+  // input: ["creator.profile"]
+  // output: schema with 'profile' key (instanceof normalizr.schema.Entity)
+  // >> effectively runs this: stack.schema.creator.schema.profile
+  return head([].concat(prev.schema[cur]));
+}
+
+/**
+ * _normalize: binds schemas to a reducing function that processes the server response
+ * into a shape that redux-query (and the rest of the redux ecosystem) works with.
+ * @param {*} schemas 
+ */
+function _normalize(schemas) {
+  return (acc, cur, ...rest) => {
+    const [key] = rest;
+    if (!schemas[pluralize(key)])
+      throw new Error(
+        `No schema exists for key "${key}". Make sure a definition exists for your resource name in model_definitions.js."`
+      );
+    // ^ @todo p3 write documentation for error
+    return merge(
+      acc,
+      normalize([].concat(cur), [schemas[pluralize(key)]]).entities
+    );
+    // ^ we concat cur to lift it into the array functor so that we can map over it later without everything breaking.
+    // ^ we pluralize the key to accommodate for single objects returned by the server with a singularized key.
+  };
+}
+
+/**
+ * _findRelatedSchemaKey: private, binds schemas and the resourceNamePlural to a
+ * reducing function that finds the ultimate base schema that a given relational
+ * property resolves to. In other words, if the reducing function sees a "creator"
+ * property, it resolves it to the "users" resource. It can do this because of the
+ * type information stored from vishnu-api's OPTIONS endpoints.
+ * @param {*} schemas 
+ * @param {*} resourceNamePlural 
+ */
+function _findRelatedSchemaKey(schemas, resourceNamePlural) {
+  return (acc, cur) => {
+    const pathToSchema = compact(cur.split('.')).map(camelize);
+    const rootSchema = schemas[resourceNamePlural];
+    const relatedSchema = reduce(pathToSchema, _walkThroughSchemas, rootSchema);
+    const relatedSchemaKey = get(head([].concat(relatedSchema)), 'key');
+
+    // We lift relatedSchema into the array functor ^^^ to account for many
+    // relationships which are also represented as [someEntity], so we can
+    // treat either argument type with the same array-oriented util functions.
+
+    // return Object.assign({}, acc, {
+    //   [relatedSchemaKey]: merge
+    // });
+    return acc.concat(relatedSchemaKey);
+  };
+}
+
+/**
+ * _toMergeMap: private, reducing function that processes
+ * an array of url fragments and returns an object that tells
+ * redux-query how to merge the newly fetched data with the collection
+ * that may or may not already be in the redux store. This merge
+ * function can be customized per request by overriding the options
+ * 
+ * see [@TODO link to gitbook.io] for more information.
+ * @param {Array} acc
+ * @param {Array} cur
+ */
+function _toMergeMap(acc, cur) {
+  return Object.assign({}, acc, { [cur]: merge });
+}
+
+/**
+ * _toQuery: private, factory function bound to normalizr schemas that
+ * were built from the type information returned from the OPTIONS
+ * endpoints of vishnu-api. It is thrice curried, as to allow it to be
+ * called with a queryExpression (built from query-builder in our queries.js
+ * file) and receive a request object in return that redux-query knows
+ * how to process.
+ * @param {*} schemas 
+ */
+function _toQuery(schema, resourceNamePlural, schemas) {
+  return (include, url) => {
+    const relatedKeys = [resourceNamePlural]
+      .concat(
+        reduce(
+          include || {},
+          _findRelatedSchemaKey(schemas, resourceNamePlural),
+          []
+        )
+      ).map(camelize)
+
+    return {
+      url,
+      transform: res => reduce(omit(res, 'meta'), _normalize(schemas), {}), // eslint-disable-line no-unused-vars
+      update: reduce(relatedKeys, _toMergeMap, {}),
+      options: {
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+    };
+  };
+}
+
+/* SCHEMAS */
+
+/**
+ * _buildGraph: private, a function that takes our base schema objects (with
+ * no knowledge of relational data) and sets up a graph of circular dependencies
+ * between all the related objects based on the model definitions returned from
+ * executing the OPTIONS method against vishnu-api's collection endpoints.
+ * @param {*} schemas 
+ * @param {*} modelDefinitions 
+ */
+function _buildGraph(baseSchemas, modelDefinitions) {
+  let graph = Object.assign({}, baseSchemas);
+
+  for (let modelDef of Object.values(modelDefinitions)) {
+    const { resource_name_plural, properties } = modelDef;
+    const resourceNamePlural = camelize(resource_name_plural);
+
+    for (let [prop_name, prop_type] of Object.entries(properties)) {
+      const { type, related_to } = prop_type;
+      const relatedTo = camelize(related_to);
+      const propName = camelize(prop_name);
+
+      if (type === 'one') {
+        graph[resourceNamePlural].define({
+          [propName]: graph[relatedTo]
+        });
+      } else if (type === 'many') {
+        graph[resourceNamePlural].define({
+          [propName]: [graph[relatedTo]]
+        });
+      }
+    }
+  }
+
+  return graph;
+}
+
+/**
+ * _buildBaseSchemas: private, a function that builds an object
+ * keyed by the `resource_name_plural` field returned from the
+ * server for a given model type. The value is a `normalizr.schema.Entity`
+ * instance with no properties defined on it.
+ * @param {*} modelDefinitions 
+ */
+function _buildBaseSchemas(modelDefinitions) {
+  return reduce(
+    modelDefinitions,
+    (acc, model_def, model_key) => {
+      const resource = camelize(model_def.resource_name_plural);
+      const modelKey = camelize(model_key);
+
+      return Object.assign({}, acc, {
+        [modelKey]: new schema.Entity(resource)
+        /*
+          Using the modelKey allows us to account for aliases (the particular case that illustrates
+          the need for this is building schemas for sideloaded objectives, which is returned from
+          drest inside a `+objectives` key.
+          
+          A little bit of an escape hatch.
+          @TODO P0 write documentation for this comment on gitbook.
+        */
+      });
+    },
+    {}
+  );
+}
+
+/**
+ * _createSchemas: private, function that takes model definitions (these
+ * currently exist in app/schemas/model_definitions.js) and returns a
+ * graph of schemas powered by normalizr, a popular redux library for
+ * defining schemas and normalizing/denormalizing data.
+ * @param {*} modelDefinitions 
+ */
+function _createSchemas(modelDefinitions) {
+  const baseSchemas = _buildBaseSchemas(modelDefinitions);
+  return _buildGraph(baseSchemas, modelDefinitions);
+}
---
+++ query-builder.js
@@ -0,0 +1,180 @@
+import { singular } from 'pluralize';
+import set from 'lodash.set';
+import last from 'lodash.last';
+import isArray from 'lodash.isarray';
+import isString from 'lodash.isstring';
+import isEmpty from 'lodash.isempty';
+import negate from 'lodash.negate';
+
+export const VALID_FILTER_OPERATORS = {
+  // IMPORTED FROM DREST
+  in: 'in',
+  any: 'any',
+  all: 'all',
+  icontains: 'icontains',
+  contains: 'contains',
+  startswith: 'startswith',
+  istartswith: 'istartswith',
+  endswith: 'endswith',
+  iendswith: 'iendswith',
+  year: 'year',
+  month: 'month',
+  day: 'day',
+  week_day: 'week_day',
+  regex: 'regex',
+  range: 'range',
+  gt: 'gt',
+  lt: 'lt',
+  gte: 'gte',
+  lte: 'lte',
+  isnull: 'isnull',
+  eq: 'eq',
+  // ALIASES
+  is: 'eq'
+};
+
+class QueryExpression {
+  constructor(
+    resource,
+    id,
+    fragments = {
+      include: [],
+      exclude: [],
+      filter: [],
+      sort: [],
+      additionalParams: {},
+      perPage: undefined
+    }
+  ) {
+    this.fragments = fragments;
+    this.fragments.id = id;
+    this.fragments.resource = resource;
+    this.fragments.resource_name_singular = singular(resource);
+    // ^ @todo p3 refactor so resource_name is singular and resource_name_plural, to match drest
+  }
+
+  _toQueryArray(parent) {
+    return (acc, cur) => {
+      if (isString(cur)) {
+        if (parent) return acc.concat(parent.concat(cur));
+        return acc.concat(cur);
+      }
+      if (isArray(cur)) {
+        const parent = last(acc);
+        return acc.concat(cur.reduce(this._toQueryArray(parent), []));
+      }
+    };
+  }
+
+  include(...args) {
+    return set(
+      this,
+      'fragments.include',
+      this.fragments.include.concat(args.reduce(this._toQueryArray(), []))
+    );
+  }
+
+  exclude(...args) {
+    return set(
+      this,
+      'fragments.exclude',
+      this.fragments.exclude.concat(args.reduce(this._toQueryArray(), []))
+    );
+  }
+
+  sort(...args) {
+    return set(
+      this,
+      'fragments.sort',
+      this.fragments.sort.concat(args.reduce(this._toQueryArray(), []))
+    );
+  }
+
+  perPage(n) {
+    return set(this, 'fragments.perPage', n);
+  }
+
+  param(param, value) {
+    return set(this, `fragments.additionalParams['${param}']`, value);
+  }
+
+  filter(entity) {
+    const proxy = new Proxy(this, {
+      get(obj, method) {
+        const symbol = VALID_FILTER_OPERATORS[method];
+        return function(term) {
+          if (symbol) {
+            obj.fragments.filter = obj.fragments.filter.concat({
+              entity,
+              term,
+              symbol
+            });
+            return obj;
+          }
+        };
+      }
+    });
+    return proxy;
+  }
+
+  // @TODO toString is looking a little long in the tooth
+  toString() {
+    let root;
+
+    if (this.fragments.id) {
+      root = '/'.concat([this.fragments.resource, this.fragments.id].join('/'));
+    } else {
+      root = '/'.concat(this.fragments.resource);
+    }
+
+    const pagination = [].concat(
+      this.fragments.perPage ? 'per_page='.concat(this.fragments.perPage) : []
+    );
+
+    const includes = this.fragments.include
+      .map(str => `include[]=${str}`)
+      .join('&');
+
+    const excludes = this.fragments.exclude
+      .map(str => `exclude[]=${str}`)
+      .join('&');
+
+    const sorts = this.fragments.sort.map(str => `sort[]=${str}`).join('&');
+
+    const filters = this.fragments.filter.reduce((acc, cur) => {
+      return `${acc.length ? acc.concat('&') : ''}`.concat(
+        // for multiple filter operations in the same Query
+        'filter{'
+          .concat(cur.entity) // entity is the collection to filter over
+          .concat(cur.symbol ? `.${cur.symbol}` : '') // symbol is the DREST filter predicate (contains, eq, gte, etc)
+          .concat('}')
+          .concat('=')
+          .concat(cur.term)
+      ); // term is the final value to filter against
+    }, '');
+
+    let additionalParams = '';
+    for (let [k, v] of Object.entries(this.fragments.additionalParams)) {
+      additionalParams = additionalParams.concat(`${k}=${v}&`).slice(0, -1);
+    }
+
+    const fragments = [
+      includes,
+      excludes,
+      filters,
+      pagination,
+      sorts,
+      additionalParams
+    ].filter(negate(isEmpty));
+
+    return root.concat('?'.concat(fragments.join('&')));
+  }
+}
+
+/* QUERY BUILDER */
+function queryFor(resource, id) {
+  // resource is a collection name, i.e. 'groups', or 'users'
+  return new QueryExpression(resource, id);
+}
+
+export default queryFor;
